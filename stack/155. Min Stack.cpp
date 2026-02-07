11b2cd69f84a4c7549957bd264c7f0c8301c0b21
jaimin <jaiminparmar200606@gmail.com>
2026-02-07 11:35:50 +0530

    155. Min Stack

---
+++ stack/155. Min Stack.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+    stack<int>st;
+    stack<int>mini;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty())
+        {
+            st.push(val);
+            mini.push(val);
+
+        }
+        else{
+            st.push(val);
+            mini.push(min(mini.top(),val));
+
+        }
+    }
+    
+    void pop() {
+        
+        st.pop();
+        mini.pop();
+    }
+    
+    int top() {
+       return st.top();
+    }
+    
+    int getMin() {
+        return mini.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
