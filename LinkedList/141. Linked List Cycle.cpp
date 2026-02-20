0996fd3d05aa8b598452ac71ff7af089a6c81daf
jaimin <jaiminparmar200606@gmail.com>
2026-02-20 15:30:05 +0530

    141. Linked List Cycle

---
+++ LinkedList/141. Linked List Cycle.cpp
@@ -0,0 +1,35 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        if(head==NULL || head->next==NULL) return false;
+        if(head->next==head) return true;
+        ListNode * slow = head,*fast = head;
+
+        while(slow && fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast)
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+        
+
+    }
+};
