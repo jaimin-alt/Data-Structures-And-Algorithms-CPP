d7f87458619ba3409004162978a3e2dec6015e8f
jaimin <jaiminparmar200606@gmail.com>
2026-03-08 22:40:38 +0530

    3210. Find the Encrypted String

---
+++ strings/3210. Find the Encrypted String.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+        int n = s.size();
+        string ans;
+        for(int i=0;i<n;i++)
+        {
+            ans+=s[(i+k)%n];
+        }
+
+        return ans;
+    }
+};
