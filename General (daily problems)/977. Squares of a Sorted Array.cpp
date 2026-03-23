b8263576ca85f30cb7919a58981ec225fa4ad2bb
jaimin <jaiminparmar200606@gmail.com>
2026-03-23 13:33:19 +0530

    977. Squares of a Sorted Array

---
+++ General (daily problems)/977. Squares of a Sorted Array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n =  nums.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = nums[i]*nums[i];
+
+        }
+
+        sort(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
