c8220458968847259baa926d0e1adc34aa369fd1
jaimin <jaiminparmar200606@gmail.com>
2026-05-27 23:40:05 +0530

    56. Merge Intervals

---
+++ pattern-wise/merge intervals/56. Merge Intervals.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>>ans;
+        
+        // sort based on first value 
+
+        sort(intervals.begin(),intervals.end(),[](vector<int>&a,vector<int>&b)
+        {
+            return a[0]<b[0];
+
+        });
+
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+
+        for(int i=1;i<n;i++)
+        {
+            int start2 = intervals[i][0];
+            int end2= intervals[i][1];
+
+            if(start2<=end1) // merge 
+            {
+                start1 = start1;
+                end1 = max(end1,end2);
+
+            }
+            else{ // no merge
+
+            ans.push_back({start1,end1});
+            start1 = start2;
+            end1 = end2;
+        
+            }
+        }
+
+        ans.push_back({start1,end1});
+        return ans;
+    }
+};
