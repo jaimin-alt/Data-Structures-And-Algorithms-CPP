fa92cc71e3ebc05878a18fa4caa0f9780065d903
jaimin <jaiminparmar200606@gmail.com>
2026-06-03 20:57:02 +0530

    Longest Subarray with Sum K

---
+++ pattern-wise/prefix sum/(GFG) Longest Subarray with Sum K.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+        int n  = arr.size();
+        unordered_map<int,int>m;
+        int ans = 0 ;
+        int sum = 0 ;
+        for(int i = 0 ;i<n;i++)
+        {
+            sum+=arr[i];
+            int x = sum-k;
+            
+            if(x==0)
+            {
+                ans = max(ans,i+1);
+                auto it = m.find(sum);
+                if(it==m.end())
+                {
+                    m[sum] = i;
+                }
+                continue;
+                
+            }
+            
+            auto it = m.find(x);
+            if(it!=m.end())
+            {
+                ans=max(ans,i-m[x]);
+            }
+            
+            if(m.find(sum)==m.end())
+            {
+                m[sum]=i;
+            }
+         
+            
+        
+            
+        }
+        
+        return ans;
+    }
+};
--- pattern-wise/prefix sum/523. Continuous Subarray Sum.cpp
+++ pattern-wise/prefix sum/523. Continuous Subarray Sum.cpp
@@ -1,10 +1,5 @@
-
-
-
 #include<bits/stdc++.h>
 using namespace std;
-
-
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
