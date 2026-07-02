52be708466f60f2cea581fb0fb0b24ac52634466
jaimin <jaiminparmar200606@gmail.com>
2026-07-02 22:07:31 +0530

    144. Binary Tree Preorder Traversal

---
+++ pattern-wise/Tree/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,40 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    #define node TreeNode
+
+    void preOrder(node*root,vector<int>&ans)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+
+        // potane store karvu 
+        ans.push_back(root->val);
+
+        // left 
+        preOrder(root->left,ans);
+
+        // right
+        preOrder(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+
+        preOrder(root,ans);
+        return ans;
+    }
+};
