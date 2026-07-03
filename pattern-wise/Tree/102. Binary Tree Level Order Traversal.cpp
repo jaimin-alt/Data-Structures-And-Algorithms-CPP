957ca2e5eee0898e86134422240cf0d5a48511ab
jaimin <jaiminparmar200606@gmail.com>
2026-07-03 17:04:38 +0530

    fixed the syntax issue

--- pattern-wise/Tree/102. Binary Tree Level Order Traversal.cpp
+++ pattern-wise/Tree/102. Binary Tree Level Order Traversal.cpp
@@ -1,18 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ #include <bits/stdc++.h>
+using namespace std;
+
+//   Definition for a binary tree node.
+    struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
 class Solution {
 public:
-    #define node TreeNode
-    #define null NULL
+    #define node TreeNode 
+    #define null NULL 
     vector<vector<int>> levelOrder(TreeNode* root) {
 
          vector<vector<int>>ans;
