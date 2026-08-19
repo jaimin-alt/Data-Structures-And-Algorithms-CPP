e8d3e4851368239d02503d5e8327ef28da46677e
jaimin <jaiminparmar200606@gmail.com>
2026-08-19 19:05:19 +0530

    110. Balanced Binary Tree

---
+++ pattern-wise/Tree/110. Balanced Binary Tree.cpp
@@ -0,0 +1,35 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+//   Definition for a binary tree node.
+    struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    bool ans = true;
+    int fun(TreeNode * root)
+    {
+        if(root==NULL) return 0;
+
+        int left = fun(root->left);
+        int right = fun(root->right);
+
+        if(abs(left-right)>1)
+        {
+            ans = false;
+        }
+        return 1+ max(left,right);
+    }
+    bool isBalanced(TreeNode* root) {
+        int height = fun(root);
+
+        return ans;
+    }
+};
